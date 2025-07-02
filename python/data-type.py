8d447e440e8ea5e9bffce0ce8eb0bc9782f46b7a
satyayanamadala <satyayanamadala99@gmail.com>
2025-07-02 17:58:46 +0530

    data type

---
+++ python/data-type.py
@@ -0,0 +1,17 @@
+name = "satya"
+cgp = 8.6
+age = 19
+passed = True
+print("This is the data type of name",type(name))
+print("This is the data type of cgp",type(cgp))
+print("This is the data type of age",type(age))
+print("This is the data type of passed",type(passed))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]] #list [...]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger")) #tuple (...)
+print(tuple1)
+
+dict1 = {"name": "Satya", "age": 19, "canVote": True} #dictionary{...}
+print(dict1)
---
+++ python/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+dict1 = {"name": "Satya", "age": 19, "canVote": True}
+print(dict1)
