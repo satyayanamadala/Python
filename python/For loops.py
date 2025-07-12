2416160b8301b9bcc6f7e13cb9ffa3676b8b99eb
satyayanamadala <satyayanamadala99@gmail.com>
2025-07-13 00:25:50 +0530

    For loop commit

---
+++ python/For loops.py
@@ -0,0 +1,18 @@
+#for loop for str
+name = "satya"
+for i in name:
+    print(i )
+    if(i=="s"):
+        print("This first letter")
+#for loop for list
+name1 = ["satyayanamadala","vatsa","mahesh"]
+for name1 in name1:
+    print(name1)
+    for i in name1:
+        print(i)
+#for range
+for j in range(7):
+    print(j)
+
+for k in range(0,10):
+    print(k+1)
